dfee4e0ad24f32506c0a75fddc80647ccd1295a3
Cedric Ziel <cedric@cedric-ziel.com>
2020-01-03 13:48:32 +0100

    Set package name and allow pushing

--- generate-clients.sh
+++ generate-clients.sh
@@ -4,7 +4,7 @@ if [[ ! -f openapi-generator-cli.jar ]]; then
 	wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.2/openapi-generator-cli-4.2.2.jar -O openapi-generator-cli.jar
 fi
 
-rm -Rf packages
+mkdir packages
 
 echo "---> Generating Bondora client for PHP"
 
@@ -12,9 +12,11 @@ if [[ ! -f bondora-v1.json ]]; then
 	wget https://api.bondora.com/swagger/docs/v1 -O bondora-v1.json
 fi
 
+git clone git@github.com:cedricziel/bondora-openapi-php.git packages/bondora-openapi-php
+
 java -jar openapi-generator-cli.jar generate -i bondora-v1.json -g php \
     -o packages/bondora-openapi-php \
-    --additional-properties=srcBasePath=src,variableNamingConvention=camelCase,invokerPackage=CedricZiel\\OpenAPI\\BondoraPHP \
+    --additional-properties=srcBasePath=src,variableNamingConvention=camelCase,invokerPackage=CedricZiel\\OpenAPI\\BondoraPHP,packageName="Bondora OpenAPI PHP Client" \
     --git-user-id=cedricziel \
     --git-repo-id=bondora-openapi-php \
     --type-mappings=object=interface{} \
@@ -22,5 +24,5 @@ java -jar openapi-generator-cli.jar generate -i bondora-v1.json -g php \
 
 pushd packages/bondora-openapi-php
     chmod +x git_push.sh
-    ./git_push.sh cedricziel bondora-openapi-php "Automated Update"
+    ./git_push.sh
 popd
